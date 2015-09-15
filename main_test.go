6ac1834c8f610e2225771a0c268f2dc0b7a9c562
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-09-15 23:48:44 +0300

    vegeta test

--- main_test.go
+++ main_test.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"testing"
+    "time"
+    "fmt"
+    "testing"
+    vegeta "github.com/tsenart/vegeta/lib"
 )
 
-func TestNewsfeedSearch(t *testing.T) {
-	NewsfeedSearch("новости")
+
+func TestMain(t *testing.T) {
+    rate := uint64(3000) // per second
+    duration := 30 * time.Second
+    targeter := vegeta.NewStaticTargeter(vegeta.Target{
+        Method: "GET",
+        URL:    "http://api.vk.com/method/users.get?user_id=1&v=5.37",
+    })
+    attacker := vegeta.NewAttacker(vegeta.Timeout(5 * time.Second))
+
+    var results vegeta.Results
+    for res := range attacker.Attack(targeter, rate, duration) {
+        results = append(results, res)
+    }
+
+    metrics := vegeta.NewMetrics(results)
+    fmt.Println(metrics)
+    fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
 }
